169beb6f40fdfa3b7ce3f95370b087456ac9d28f
Iosif V <dev@iosifv.com>
2022-09-01 14:17:53 +0100

    lesson 14 - dynamic nfts - finished writing code

--- patrick-collins-tutorials/hardhat-fcc/hardhat-nft/contracts/DynamicSvgNft.sol
+++ patrick-collins-tutorials/hardhat-fcc/hardhat-nft/contracts/DynamicSvgNft.sol
@@ -3,9 +3,9 @@
 pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "base64-sol/base64.sol"
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "base64-sol/base64.sol";
 
-// import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 // import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -13,10 +13,21 @@ contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
     string private i_lowImageUri;
     string private i_highImageUri;
-    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64;"
-    
-    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValue;
+
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+        i_lowImageUri = svgToSvgUri(lowSvg);
+        i_highImageUri = svgToSvgUri(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     function svgToSvgUri() public pure returns (string memory) {
@@ -24,18 +35,48 @@ contract DynamicSvgNft is ERC721 {
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
     }
 
-    function mintNft() public {
-        _safeMint(msg.sender, s_tokenCounter);
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValue[s_tokenCounter] = highValue;
         s_tokenCounter = s_tokenCounter + 1;
+        _safeMint(msg.sender, s_tokenCounter);
+        emit CreatedNFT(s_tokenCounter, highValue);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // the _exists function comes from the ERC721
+        require(_exists(tokenId), "URI Query for nonexistent token");
+
+        // data:image/svg+xml;base64,
+        // data:application/json;base64,
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageUri = i_lowImageUri;
+        if (price >= s_tokenIdToHighValue[tokenId]) {
+            imageUri = i_highImageUri;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":',
+                                name(),
+                                '", "description":"An NFT which changes based on the Chainlink Feed"',
+                                '"attributes": [{"trait_type":"coolness", "value": 100}],',
+                                '"image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
-
-
-
-
-
-
-
-
-
-
