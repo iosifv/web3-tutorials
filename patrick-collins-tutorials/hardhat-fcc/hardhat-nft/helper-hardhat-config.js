29695d295f82e31747ecab257c42416c95c19b7d
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-04 21:22:02 +0500

    Corrected the eth/usd price feed in goerli
    Corrected the eth/usd price feed in goerli

--- patrick-collins-tutorials/hardhat-fcc/hardhat-nft/helper-hardhat-config.js
+++ patrick-collins-tutorials/hardhat-fcc/hardhat-nft/helper-hardhat-config.js
@@ -22,7 +22,7 @@ const networkConfig = {
         callbackGasLimit: "500000",
         interval: "30",
         mintFee: "10000000000000000", // 0.01 ETH
-        ethUsdPriceFeedAddress: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        ethUsdPriceFeedAddress: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
     },
     31337: {
         name: "hardhat",
